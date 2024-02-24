358518bc026ed497bbb271216e4089b59fe37665
Dalvan Griebler <dalvangriebler@gmail.com>
2024-02-24 10:13:47 -0300

    add: master slave for python

--- MPI/master_slave.py
+++ MPI/master_slave.py
@@ -2,12 +2,13 @@ from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-
+numProc = comm.Get_size()
 
 if rank == 0:
-    data=["Maikel", "Vagner", "Gabriel"]
-    comm.send(data, dest=1)
+    dado = "eu "
+    for destino in range(1, numProc):
+        comm.send(dado+str(destino), dest=destino)
 
 else:
     data = comm.recv(source=0)
-    print(data)
+    print("I am ", rank, "Received: ", data)
